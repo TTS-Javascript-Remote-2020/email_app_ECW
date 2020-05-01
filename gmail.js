d99a65a29d897fd3c6900211f941df2bb79303b7
Elliott Woodward <elliottcwoodward@gmail.com>
2020-05-01 10:41:35 -0400

    Add main JS source file with initial object variable

---
+++ gmail.js
@@ -0,0 +1,18 @@
+var appData = {
+  "name": "gmail",
+  "mailboxes": [
+    'inbox',
+    'starred',
+    'sent',
+  ],
+  "contacts": [
+    {"name": "JaSON"
+    "lastMessage": "I am Javascript Object Notation"},
+    {"name": "Alice",
+    "lastMessage": "This should be encrypted"},
+    {"name": "Bob",
+    "lastMessage": "I received an encrypted message from Alice"},
+    {"name": "Mallory",
+    "lastMessage": "I intercepted an encrypted message between Alice and Bob"},
+  ]
+};
